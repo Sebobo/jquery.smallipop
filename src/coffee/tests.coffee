58f93567ef05ac4161bf4b59803ccdbf755ddddf
Sebastian Helzle <sebastian@helzle.net>
2013-02-09 16:25:34 +0100

    More smallpox api tests

--- src/coffee/tests.coffee
+++ src/coffee/tests.coffee
@@ -13,12 +13,12 @@ test 'Smallipop exists', ->
   equal smallipop.attr('id'), 'smallipop1', 'First smallipop should have id 1'
 
 
-asyncTest 'Show tooltip', ->
+asyncTest 'Show and hide tooltip by interaction', ->
   smallipop = $ '.smallipop-instance'
   trigger = $ '.smallipop:first'
 
   # Check if lightbox is created and shown by calling the api
-  trigger.smallipop 'show'
+  trigger.trigger 'mouseenter'
 
   # Try closing the smallipop by clicking the document
   setTimeout( ->
@@ -32,4 +32,46 @@ asyncTest 'Show tooltip', ->
       # Resume testrunner
       start()
     , 300)
-  , 100)
+  , 200)
+
+
+asyncTest 'Show and hide tooltip with api', ->
+  smallipop = $ '.smallipop-instance'
+  trigger = $ '.smallipop:first'
+
+  # Check if lightbox is created and shown by calling the api
+  trigger.smallipop 'show'
+
+  # Try closing the smallipop by clicking the document
+  setTimeout( ->
+    ok smallipop.is(':visible'), 'Smallipop should be visible'
+
+    trigger.smallipop 'hide'
+
+    setTimeout( ->
+      equal smallipop.css('display'), 'none', 'Smallipop should now be hidden'
+
+      # Resume testrunner
+      start()
+    , 300)
+  , 200)
+
+
+asyncTest 'Change tooltip content with api', ->
+  smallipop = $ '.smallipop-instance'
+  trigger = $ '.smallipop:first'
+
+  oldHint = trigger.data('smallipop').hint
+  newHint = 'some fancy new hint'
+
+  notEqual oldHint, newHint
+
+  # Update the smallipop hint
+  trigger.smallipop 'update', newHint
+
+  setTimeout( ->
+    equal $('.sipContent', smallipop).text(), newHint, 'Hint should have changed'
+
+    # Resume testrunner
+    start()
+  , 200)
--- tests/tests.js
+++ tests/tests.js
@@ -11,11 +11,11 @@ test('Smallipop exists', function() {
   return equal(smallipop.attr('id'), 'smallipop1', 'First smallipop should have id 1');
 });
 
-asyncTest('Show tooltip', function() {
+asyncTest('Show and hide tooltip by interaction', function() {
   var smallipop, trigger;
   smallipop = $('.smallipop-instance');
   trigger = $('.smallipop:first');
-  trigger.smallipop('show');
+  trigger.trigger('mouseenter');
   return setTimeout(function() {
     ok(smallipop.is(':visible'), 'Smallipop should be visible');
     $(document).trigger('click');
@@ -23,5 +23,34 @@ asyncTest('Show tooltip', function() {
       equal(smallipop.css('display'), 'none', 'Smallipop should now be hidden');
       return start();
     }, 300);
-  }, 100);
+  }, 200);
+});
+
+asyncTest('Show and hide tooltip with api', function() {
+  var smallipop, trigger;
+  smallipop = $('.smallipop-instance');
+  trigger = $('.smallipop:first');
+  trigger.smallipop('show');
+  return setTimeout(function() {
+    ok(smallipop.is(':visible'), 'Smallipop should be visible');
+    trigger.smallipop('hide');
+    return setTimeout(function() {
+      equal(smallipop.css('display'), 'none', 'Smallipop should now be hidden');
+      return start();
+    }, 300);
+  }, 200);
+});
+
+asyncTest('Change tooltip content with api', function() {
+  var newHint, oldHint, smallipop, trigger;
+  smallipop = $('.smallipop-instance');
+  trigger = $('.smallipop:first');
+  oldHint = trigger.data('smallipop').hint;
+  newHint = 'some fancy new hint';
+  notEqual(oldHint, newHint);
+  trigger.smallipop('update', newHint);
+  return setTimeout(function() {
+    equal($('.sipContent', smallipop).text(), newHint, 'Hint should have changed');
+    return start();
+  }, 200);
 });
